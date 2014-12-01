577f986663acfa8ebc6dd26d9c680a99a8e35d75
Ignacio Santolin <isantolin@users.noreply.github.com>
2014-12-01 13:29:48 -0300

    replaced die()

--- example/server/php/record.php
+++ example/server/php/record.php
@@ -5,6 +5,11 @@ parse_str($_SERVER['QUERY_STRING'], $params);
 $file = isset($params['filename']) ? $params['filename'] : 'temp.wav';
 // save the recorded audio to that file
 $content = file_get_contents('php://input');
-$fh = fopen($file, 'w') || exit("can't open file");
-fwrite($fh, $content);
-fclose($fh);
+$fh = fopen($file, 'w');
+
+if ($fh != FALSE) {
+    fwrite($fh, $content);
+    fclose($fh);
+} else {
+    echo "can't open file";
+}
